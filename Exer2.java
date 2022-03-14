92a20559a4c2ae14cefc5eebc8b1868fa6b141fb
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-14 18:45:33 +0000

    imprimindo a soma de 1 a 1000

---
+++ Exer2.java
@@ -0,0 +1,12 @@
+public class Exer2{
+    public static void main(String[] args){
+    int inicio=1;
+	double fim=1000;
+	double operacao = (inicio + fim) * (fim/2);
+	int resultado = (int)operacao;
+
+	System.out.println(resultado);
+
+    
+}
+}
